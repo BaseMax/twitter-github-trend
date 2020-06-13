eb4aa11ec20cb6194a38f7a3a6b9a41c216f06a3
Max Base <MaxBaseCode@Gmail.Com>
2020-06-13 07:59:36 +0430

    Update send.php

--- send.php
+++ send.php
@@ -5,17 +5,17 @@ include "config.php";
 
 $repos=[];
 $res=get("https://github.com/trending");
+
 preg_match_all('/<h1 class="h3 lh-condensed">(\s*|)<a href=\"\/([^\/]+)\/([^\"]+)" data-hydro-click/i', $res[0], $_repos);
+
 preg_match_all('/<p class="col-9 text-gray my-1 pr-4">(.*?|)<\/p>/is', $res[0], $_descs);
-//print_r($_repos);
-// print_r($_descs[1]);
 $_descs=$_descs[1];
 foreach($_descs as $i=>$_desc) {
 	$_descs[$i]=strip_tags($_descs[$i]);
 	$_descs[$i]=html_entity_decode($_descs[$i]);
 	$_descs[$i]=trim($_descs[$i]);
 }
-// print_r($_descs);
+
 if(!file_exists("db.txt")) {
 	file_put_contents("db.txt", "");
 }
@@ -33,11 +33,8 @@ foreach($_repos[2] as $i=>$_repo) {
 	else {
 		$desc=$_descs[$index];
 	}
-	// $_descs[$i][1]=trim(html_entity_decode(strip_tags($_descs[$i][1])));
 	$pat='/<span itemprop=\"programmingLanguage\">([^<]+|)<\/span>(\s*|)<\/span>(\s*|)<a class=\"(\s*|)muted-link d-inline-block mr-3\" href="\/'.str_replace("/", "\/", $rep).'/is';
-	// print $pat."\n";
 	preg_match($pat, $res[0], $_lang);
-	// print_r($_lang);
 	if($rep != "") {
 		$repos[]=[
 			"language"=>isset($_lang[1]) ? $_lang[1] : "",
@@ -77,4 +74,3 @@ foreach($repos as $repo) {
 		}
 	}
 }
-
