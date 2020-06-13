c92a2b60b57095e715fd13a2e2acdd57da742b7e
Max Base <MaxBaseCode@Gmail.Com>
2020-06-13 07:50:44 +0430

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Max Base
+// https://github.com/BaseMax/twitter-github-trend
+use DG\Twitter\Twitter;
+require_once "NetPHP.php";
+require_once "twitter.class.php";
+
+// https://developer.twitter.com/en/account/get-started
+// https://developer.twitter.com/en/apps/
+$twitter = new Twitter("**********", "**********", "**********-**********", "**********"); // need to generate API
